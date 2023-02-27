ac6ada6324813a3f60190fb2edc84382e8e7a10e
Nur Ajjrina <NurAjjrina.BintiAzmi@amd.com>
2023-02-27 07:55:52 +0000

    Make a change to demonstrate Poll SCM

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Jenkins is cool XD");
+        System.out.println("Jenkins is okayla");
     }
 
     public String getMessage() {
