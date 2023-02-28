18f09d311e8d961b88ed90d0525d8bde4fdb121a
Nur Ajjrina <NurAjjrina.BintiAzmi@amd.com>
2023-02-28 05:43:54 +0000

    change something in pipelines scm, one more time

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Testing pipelines scm~~");
+        System.out.println("Testing pipelines scm~~one more time");
     }
 
     public String getMessage() {
